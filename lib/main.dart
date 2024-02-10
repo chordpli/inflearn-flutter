a69ab8b09b07ece757f2ad3908bb331a8147a0a1
chordpli <chordplaylist@gmail.com>
2024-02-11 00:00:37 +0900

    feat: 머티리얼 디자인

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.brown,
       ),
       home: const MainPage(),
     );
   }
 }
-
